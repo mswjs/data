96e17501250c59c2e5f26684795c710ad511f82c
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-10 14:49:18 +0100

    Structures relational tests

---
+++ test/relations/one-to-one.test.ts
@@ -0,0 +1,55 @@
+import { random } from 'faker'
+import { factory, oneOf } from '../../src/factory'
+
+test('supports one-to-one relation', () => {
+  const db = factory({
+    country: {
+      name: random.words,
+    },
+    capital: {
+      name: random.word,
+      country: oneOf('country'),
+    },
+  })
+
+  const usa = db.country.create({
+    name: 'United States of America',
+  })
+  const washington = db.capital.create({
+    name: 'Washington',
+    country: usa,
+  })
+
+  expect(washington.country).toHaveProperty('name', 'United States of America')
+})
+
+test('supports querying through a one-to-one relational property', () => {
+  const db = factory({
+    country: {
+      name: random.words,
+    },
+    capital: {
+      name: random.word,
+      country: oneOf('country'),
+    },
+  })
+
+  const usa = db.country.create({
+    name: 'United States of America',
+  })
+  const washington = db.capital.create({
+    name: 'Washington',
+    country: usa,
+  })
+
+  const capital = db.capital.findOne({
+    which: {
+      country: {
+        name: {
+          equals: 'United States of America',
+        },
+      },
+    },
+  })
+  expect(capital).toHaveProperty('name', 'Washington')
+})
