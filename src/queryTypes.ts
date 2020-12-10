52e0c90f0ffc4bec13ad9ca8bfa073a703e48457
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-10 13:47:29 +0100

    Supports querying through relational properties

--- src/queryTypes.ts
+++ src/queryTypes.ts
@@ -1,15 +1,25 @@
+import { Value, OneOf } from './factory'
+
 export interface QuerySelector<EntityType extends Record<string, any>> {
   which: {
-    [P in keyof EntityType]?: Partial<GetQueryFor<EntityType[P]>>
+    [K in keyof EntityType]?: Partial<GetQueryFor<EntityType[K]>>
   }
 }
 
-export type GetQueryFor<T extends string | number | any[]> = T extends string
+export type GetQueryFor<
+  T extends string | number | any[] | OneOf<any>
+> = T extends string
   ? StringQuery
   : T extends number
   ? NumberQuery
   : T extends Array<infer U>
   ? QuerySelector<U>['which']
+  : /**
+   * Relational `oneOf`/`manyOf` invocation
+   * resolves to the `Value` type.
+   */
+  T extends Value<any, any>
+  ? QuerySelector<T>['which']
   : never
 
 export interface StringQuery {
--- test/relation.test.ts
+++ test/relation.test.ts
@@ -1,5 +1,6 @@
-import { random } from 'faker'
+import { random, name } from 'faker'
 import { factory, oneOf } from '../src/factory'
+import { identity } from '../src/utils/identity'
 
 test('supports one-to-one relation', () => {
   const db = factory({
@@ -22,4 +23,111 @@ test('supports one-to-one relation', () => {
   })
 
   expect(washington.country).toHaveProperty('name', 'United States of America')
+})
+
+test('supports querying against a relational property', () => {
+  const userId = random.uuid()
+  const db = factory({
+    user: {
+      id: identity(userId),
+      firstName: name.firstName,
+    },
+    post: {
+      title: random.words,
+      author: oneOf('user'),
+    },
+  })
+
+  const user = db.user.create()
+  db.post.create({
+    title: 'First post',
+    author: user,
+  })
+  db.post.create({
+    title: 'Second post',
+    author: user,
+  })
+  db.post.create({
+    title: 'Third post',
+    author: user,
+  })
+
+  const userPosts = db.post.findMany({
+    which: {
+      author: {
+        id: {
+          equals: userId,
+        },
+      },
+    },
+  })
+
+  expect(userPosts).toHaveLength(3)
+  const postTitles = userPosts.map((post) => post.title)
+  expect(postTitles).toEqual(['First post', 'Second post', 'Third post'])
+})
+
+test('supports querying against nested relational properties', () => {
+  const db = factory({
+    role: {
+      title: random.word,
+    },
+    user: {
+      firstName: name.firstName,
+      role: oneOf('role'),
+    },
+    post: {
+      title: random.words,
+      author: oneOf('user'),
+    },
+  })
+
+  const editor = db.role.create({
+    title: 'Editor',
+  })
+  const reader = db.role.create({
+    title: 'Reader',
+  })
+
+  const firstUser = db.user.create({
+    firstName: 'John',
+    role: editor,
+  })
+  const secondUser = db.user.create({
+    role: reader,
+  })
+  db.user.create({
+    role: reader,
+  })
+
+  db.post.create({
+    title: 'First post',
+    author: firstUser,
+  })
+  db.post.create({
+    title: 'Second post',
+    author: firstUser,
+  })
+  db.post.create({
+    title: 'Third post',
+    author: firstUser,
+  })
+  db.post.create({
+    title: 'Fourth post',
+    author: secondUser,
+  })
+
+  const result = db.post.findMany({
+    which: {
+      author: {
+        role: {
+          title: {
+            equals: 'Editor',
+          },
+        },
+      },
+    },
+  })
+
+  expect(result).toHaveLength(3)
 })
