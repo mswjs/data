b7731a935d881d173d39ba6597fd6b6047914722
marcosvega91 <marcomoretti0103@gmail.com>
2020-12-15 14:01:50 +0100

    test: test every method for string

--- test/query/string.test.ts
+++ test/query/string.test.ts
@@ -1,65 +1,52 @@
 import { factory } from '../../src'
 
-test('queries entity which property equals a string', () => {
-  const db = factory({
-    user: {
-      firstName: String,
-      role: String,
-    },
-  })
-  db.user.create({
-    firstName: 'John',
-    role: 'editor',
-  })
-  db.user.create({
-    firstName: 'Kate',
-    role: 'admin',
-  })
-  db.user.create({
-    firstName: 'Alice',
-    role: 'reader',
-  })
-  db.user.create({
-    firstName: 'Joseph',
-    role: 'editor',
-  })
-
-  const firstEditor = db.user.findFirst({
-    which: {
-      role: {
-        equals: 'editor',
-      },
-    },
-  })
-  expect(firstEditor).toHaveProperty('firstName', 'John')
-
-  const allEditors = db.user.findMany({
-    which: {
-      role: {
-        equals: 'editor',
-      },
-    },
-  })
-  expect(allEditors).toHaveLength(2)
-  const editorNames = allEditors.map((user) => user.firstName)
-  expect(editorNames).toEqual(['John', 'Joseph'])
-})
-
-test('queries entities which property contains a string', () => {
+const setup = () => {
   const db = factory({
     recipe: {
       title: String,
+      category: String,
     },
   })
   db.recipe.create({
     title: 'New York Pizza',
+    category: 'pizza',
   })
   db.recipe.create({
     title: 'Chocolate Cake',
+    category: 'cake',
   })
   db.recipe.create({
     title: 'Pizza Mozzarrela',
+    category: 'pizza',
   })
+  return db
+}
+test('queries entity which property equals a string', () => {
+  const db = setup()
+
+  const firstPizza = db.recipe.findFirst({
+    which: {
+      category: {
+        equals: 'pizza',
+      },
+    },
+  })
+  expect(firstPizza).toHaveProperty('title', 'New York Pizza')
+
+  const allPizza = db.recipe.findMany({
+    which: {
+      category: {
+        equals: 'pizza',
+      },
+    },
+  })
+  expect(allPizza).toHaveLength(2)
+  const titles = allPizza.map((pizza) => pizza.title)
+  expect(titles).toEqual(['New York Pizza', 'Pizza Mozzarrela'])
+})
+
+test('queries entities which property contains a string', () => {
+  const db = setup()
 
   const firstPizza = db.recipe.findFirst({
     which: {
@@ -80,4 +67,56 @@ test('queries entities which property contains a string', () => {
   expect(allPizzas).toHaveLength(2)
   const pizzaTitles = allPizzas.map((pizza) => pizza.title)
   expect(pizzaTitles).toEqual(['New York Pizza', 'Pizza Mozzarrela'])
+})
+
+test('queries entities which property not contains a string', () => {
+  const db = setup()
+
+  const chocolateCake = db.recipe.findFirst({
+    which: {
+      title: {
+        notContains: 'Pizza',
+      },
+    },
+  })
+  expect(chocolateCake).toHaveProperty('title', 'Chocolate Cake')
+})
+
+test('queries entities which property is not equals to a string', () => {
+  const db = setup()
+
+  const chocolateCake = db.recipe.findFirst({
+    which: {
+      title: {
+        notEquals: 'New York Pizza',
+      },
+    },
+  })
+  expect(chocolateCake).toHaveProperty('title', 'Chocolate Cake')
+})
+
+test('queries entities which property is not contained into the array', () => {
+  const db = setup()
+
+  const chocolateCake = db.recipe.findFirst({
+    which: {
+      title: {
+        notIn: ['New York Pizza'],
+      },
+    },
+  })
+  expect(chocolateCake).toHaveProperty('title', 'Chocolate Cake')
+})
+
+test('queries entities which property is contained into the array', () => {
+  const db = setup()
+
+  const chocolateCake = db.recipe.findFirst({
+    which: {
+      title: {
+        in: ['New York Pizza'],
+      },
+    },
+  })
+  expect(chocolateCake).toHaveProperty('title', 'New York Pizza')
 })
