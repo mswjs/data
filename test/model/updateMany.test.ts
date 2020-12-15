f0e9e47a1d70249e5d8d5ec661a8d8366d872466
marcosvega91 <marcomoretti0103@gmail.com>
2020-12-15 16:13:42 +0100

    feat: add updateMany method
    close #10

---
+++ test/model/updateMany.test.ts
@@ -0,0 +1,79 @@
+import { random, name } from 'faker'
+import { factory } from '../../src'
+import { identity } from '../../src/utils/identity'
+
+test('should update many entity with evolution value', () => {
+  const db = factory({
+    user: {
+      id: identity(random.uuid()),
+      firstName: name.findName,
+      role: String,
+    },
+  })
+  db.user.create({
+    firstName: 'Joseph',
+    role: 'Auditor',
+  })
+
+  db.user.create({
+    firstName: 'John',
+    role: 'Auditor',
+  })
+
+  const updateMultiUsers = db.user.updateMany({
+    which: {
+      role: {
+        equals: 'Auditor',
+      },
+    },
+    data: {
+      firstName: (firstName) => firstName.toUpperCase(),
+    },
+  })
+
+  expect(updateMultiUsers).toHaveLength(2)
+  const names = updateMultiUsers.map((user) => user.firstName)
+  expect(names).toEqual(['JOSEPH', 'JOHN'])
+
+  const userResult = db.user.findFirst({
+    which: {
+      role: {
+        equals: 'Auditor',
+      },
+    },
+  })
+  expect(userResult).toHaveProperty('firstName', 'JOSEPH')
+})
+
+test('should update many entities with primitive values', () => {
+  const db = factory({
+    user: {
+      id: identity(random.uuid()),
+      firstName: name.findName,
+      role: String,
+    },
+  })
+  db.user.create({
+    firstName: 'Joseph',
+    role: 'Auditor',
+  })
+
+  db.user.create({
+    firstName: 'John',
+    role: 'Auditor',
+  })
+
+  const updateMultiUsers = db.user.updateMany({
+    which: {
+      role: {
+        equals: 'Auditor',
+      },
+    },
+    data: {
+      role: 'Administrator',
+    },
+  })
+
+  expect(updateMultiUsers).toHaveLength(2)
+  updateMultiUsers.forEach((user) => expect(user.role).toEqual('Administrator'))
+})
