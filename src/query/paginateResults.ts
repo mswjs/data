95b69770348ce7eb0e254fe03c46bf6e55fc77ea
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-22 18:19:04 +0100

    Supports cursor-based pagination

--- src/query/paginateResults.ts
+++ src/query/paginateResults.ts
@@ -1,12 +1,26 @@
 import { EntityInstance } from '../glossary'
 import { BulkQueryOptions, QuerySelector } from './queryTypes'
 
+function getEndIndex(start: number, end?: number) {
+  return end ? start + end : undefined
+}
+
 export function paginateResults(
   query: QuerySelector<any> & BulkQueryOptions,
   data: EntityInstance<any, any>[],
 ): EntityInstance<any, any>[] {
-  const start = query.skip || 0
-  const end = query.take ? start + query.take : undefined
+  if (query.cursor) {
+    const cursorIndex = data.findIndex((entity) => {
+      return entity[entity.__primaryKey] === query.cursor
+    })
 
-  return data.slice(start, end)
+    if (cursorIndex === -1) {
+      return []
+    }
+
+    return data.slice(cursorIndex + 1, getEndIndex(cursorIndex + 1, query.take))
+  }
+
+  const start = query.skip || 0
+  return data.slice(start, getEndIndex(start, query.take))
 }
