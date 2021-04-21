cf7f659e1181b77f779c336916ac14b11b419ffa
Artem Zakharchenko <kettanaito@gmail.com>
2021-04-21 11:29:54 +0200

    defineRelationalProperties: Extends native "PropertyDescriptor" to annotate custom defined properties

--- src/model/defineRelationalProperties.ts
+++ src/model/defineRelationalProperties.ts
@@ -13,6 +13,12 @@ import { first } from '../utils/first'
 
 const log = debug('defineRelationalProperties')
 
+type RelationalPropertyDescriptorMap = {
+  [property: string]: Omit<PropertyDescriptor, 'get'> & {
+    get(): InternalEntity<any, any> | InternalEntity<any, any>[]
+  }
+}
+
 export function defineRelationalProperties(
   entity: InternalEntity<any, any>,
   initialValues: Partial<Value<any, ModelDictionary>>,
@@ -21,97 +27,97 @@ export function defineRelationalProperties(
 ): void {
   log('setting relations', relations, entity)
 
-  const properties = Object.entries(relations).reduce<
-    Record<
-      string,
-      {
-        get(): InternalEntity<any, any> | InternalEntity<any, any>[]
-        enumerable: boolean
-      }
-    >
-  >((properties, [property, relation]) => {
-    log(`defining relational property "${entity.__type}.${property}"`, relation)
-    // Take the relational entity reference from the initial values.
-    const entityRefs: Entity<any, any>[] = [].concat(initialValues[property])
+  const properties = Object.entries(
+    relations,
+  ).reduce<RelationalPropertyDescriptorMap>(
+    (properties, [property, relation]) => {
+      log(
+        `defining relational property "${entity.__type}.${property}"`,
+        relation,
+      )
+      // Take the relational entity reference from the initial values.
+      const entityRefs: Entity<any, any>[] = [].concat(initialValues[property])
 
-    if (relation.unique) {
-      log(`verifying that the "${property}" relation is unique...`)
+      if (relation.unique) {
+        log(`verifying that the "${property}" relation is unique...`)
 
-      /**
-       * @fixme Is it safe to assume the first reference?
-       */
-      const firstRef = entityRefs[0]
+        /**
+         * @fixme Is it safe to assume the first reference?
+         */
+        const firstRef = entityRefs[0]
 
-      // Trying to look up an entity of the same type
-      // that references the same relational entity.
-      const existingEntities = executeQuery(
-        entity.__type,
-        entity.__primaryKey,
-        {
-          where: {
-            [property]: {
-              [relation.primaryKey]: {
-                in: entityRefs.map(
-                  (entityRef) => entityRef[entity.__primaryKey],
-                ),
+        // Trying to look up an entity of the same type
+        // that references the same relational entity.
+        const existingEntities = executeQuery(
+          entity.__type,
+          entity.__primaryKey,
+          {
+            where: {
+              [property]: {
+                [relation.primaryKey]: {
+                  in: entityRefs.map(
+                    (entityRef) => entityRef[entity.__primaryKey],
+                  ),
+                },
               },
             },
           },
-        },
-        db,
-      )
-
-      log(
-        `existing entities that reference the same "${property}"`,
-        existingEntities,
-      )
-
-      if (existingEntities.length > 0) {
-        log(`found a non-unique relational entity!`)
-
-        throw new Error(
-          `Failed to create a unique "${relation.modelName}" relation for "${
-            entity.__type
-          }.${property}" (${
-            entity[entity.__primaryKey]
-          }): the provided entity is already used.`,
+          db,
         )
+
+        log(
+          `existing entities that reference the same "${property}"`,
+          existingEntities,
+        )
+
+        if (existingEntities.length > 0) {
+          log(`found a non-unique relational entity!`)
+
+          throw new Error(
+            `Failed to create a unique "${relation.modelName}" relation for "${
+              entity.__type
+            }.${property}" (${
+              entity[entity.__primaryKey]
+            }): the provided entity is already used.`,
+          )
+        }
       }
-    }
 
-    properties[property] = {
-      enumerable: true,
-      get() {
-        log(`get "${property}"`, relation)
+      properties[property] = {
+        enumerable: true,
+        get() {
+          log(`get "${property}"`, relation)
 
-        const refValue = entityRefs.reduce<InternalEntity<any, any>[]>(
-          (list, entityRef) => {
-            return list.concat(
-              executeQuery(
-                relation.modelName,
-                relation.primaryKey,
-                {
-                  where: {
-                    [relation.primaryKey]: {
-                      equals: entityRef[relation.primaryKey],
+          const refValue = entityRefs.reduce<InternalEntity<any, any>[]>(
+            (list, entityRef) => {
+              return list.concat(
+                executeQuery(
+                  relation.modelName,
+                  relation.primaryKey,
+                  {
+                    where: {
+                      [relation.primaryKey]: {
+                        equals: entityRef[relation.primaryKey],
+                      },
                     },
                   },
-                },
-                db,
-              ),
-            )
-          },
-          [],
-        )
-        log(`resolved "${relation.kind}" "${property}" to`, refValue)
+                  db,
+                ),
+              )
+            },
+            [],
+          )
+          log(`resolved "${relation.kind}" "${property}" to`, refValue)
 
-        return relation.kind === RelationKind.OneOf
-          ? first(refValue)!
-          : refValue
-      },
-    }
+          return relation.kind === RelationKind.OneOf
+            ? first(refValue)!
+            : refValue
+        },
+      }
 
-    return properties
-  }, {})
+      return properties
+    },
+    {},
+  )
   Object.defineProperties(entity, properties)
 }
