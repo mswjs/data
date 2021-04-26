38020c37d485c1f5be35bfdd6eae78cb2d46d4c5
Luca Barone <baro.luc@gmail.com>
2021-04-26 14:33:47 +0200

    Apply type guards on filter and forEach utils

--- src/utils/iteratorUtils.ts
+++ src/utils/iteratorUtils.ts
@@ -1,4 +1,6 @@
-export function forEach<K, V>(
+import { PrimaryKeyType } from "../glossary"
+
+export function forEach<K extends PrimaryKeyType, V>(
   fn: (key: K, value: V) => any,
   map: Map<K, V>,
 ): void {
@@ -7,7 +9,7 @@ export function forEach<K, V>(
   }
 }
 
-export function filter<K, V>(
+export function filter<K extends PrimaryKeyType, V>(
   predicate: (key: K, value: V) => boolean,
   map: Map<K, V>,
 ): Map<K, V> {
