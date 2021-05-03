ed55dea3e25e470dfa076115c52516038b23668a
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-03 16:03:57 +0200

    Rewrites the "manyOf" update regression test

--- test/regressions/02-handlers-many-of.test.ts
+++ test/regressions/02-handlers-many-of.test.ts
@@ -1,19 +1,7 @@
 import fetch from 'node-fetch'
-import { datatype, name, random } from 'faker'
+import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { factory, manyOf, primaryKey } from '@mswjs/data'
-
-const db = factory({
-  user: {
-    id: primaryKey(datatype.uuid),
-    firstName: name.firstName,
-    posts: manyOf('post'),
-  },
-  post: {
-    id: primaryKey(datatype.uuid),
-    title: random.words,
-  },
-})
 
 const server = setupServer()
 
@@ -26,51 +14,121 @@ afterAll(() => {
 })
 
 it('updates database entity modified via a generated handler', async () => {
-  const firstPost = db.post.create({
-    title: 'First post',
-  })
-  const secondPost = db.post.create({
-    title: 'Second post',
+  const db = factory({
+    user: {
+      id: primaryKey(String),
+      notes: manyOf('note'),
+    },
+    note: {
+      id: primaryKey(String),
+      title: String,
+    },
   })
 
-  // Create a database entity outside of the generated handlers.
-  // Bind it to a "mayOf" posts relationship.
   db.user.create({
-    id: 'abc-123',
-    firstName: 'John',
-    posts: [firstPost],
+    id: 'user-1',
+    notes: [
+      db.note.create({ id: 'note-1', title: 'First note' }),
+      db.note.create({ id: 'note-2', title: 'Second note' }),
+    ],
   })
 
-  server.use(...db.user.toHandlers('rest', 'http://localhost'))
+  server.use(
+    rest.get('/user', (req, res, ctx) => {
+      const user = db.user.findFirst({
+        strict: true,
+        where: {
+          id: {
+            equals: 'user-1',
+          },
+        },
+      })
+      return res(ctx.json(user))
+    }),
+    rest.put<{ title: string }>('/note/:noteId', (req, res, ctx) => {
+      const { noteId } = req.params
 
-  // Update the previously populated "user" instance
-  // via the generated request handlers.
-  const response = await fetch('http://localhost/users/abc-123', {
+      const updatedNote = db.note.update({
+        strict: true,
+        where: {
+          id: {
+            equals: noteId,
+          },
+        },
+        data: {
+          title: req.body.title,
+        },
+      })
+
+      return res(ctx.json(updatedNote))
+    }),
+  )
+
+  // Update a referenced relational property via request handler.
+  const noteUpdateResponse = await fetch('http://localhost/note/note-2', {
     method: 'PUT',
-    // Pay attention to compose a proper request,
-    // including its "Content-Type" header.
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      posts: [secondPost],
+      title: 'Updated title',
     }),
   })
-  const json = await response.json()
-  expect(response.status).toEqual(200)
-  expect(json).toEqual({
-    id: 'abc-123',
-    firstName: 'John',
-    posts: [secondPost],
+  expect(noteUpdateResponse.status).toEqual(200)
+
+  // Updates persist when querying the updated entity directly.
+  expect(
+    db.note.findFirst({
+      where: {
+        id: {
+          equals: 'note-2',
+        },
+      },
+    }),
+  ).toEqual({
+    id: 'note-2',
+    title: 'Updated title',
   })
 
-  // Query the database directly.
-  const user = db.user.findFirst({
-    where: {
-      id: {
-        equals: 'abc-123',
+  // Updates persist when querying a parent entity that references
+  // the updated relational entity.
+  expect(
+    db.user.findFirst({
+      where: {
+        id: {
+          equals: 'user-1',
+        },
       },
-    },
+    }),
+  ).toEqual({
+    id: 'user-1',
+    notes: [
+      {
+        id: 'note-1',
+        title: 'First note',
+      },
+      {
+        id: 'note-2',
+        title: 'Updated title',
+      },
+    ],
   })
-  expect(user).toHaveProperty('posts', [secondPost])
+
+  //
+  const refetchedUser = await fetch('http://localhost/user').then((res) =>
+    res.json(),
+  )
+  expect(refetchedUser).toEqual({
+    id: 'user-1',
+    notes: [
+      {
+        id: 'note-1',
+        title: 'First note',
+      },
+      {
+        id: 'note-2',
+        title: 'Updated title',
+      },
+    ],
+  })
 })
