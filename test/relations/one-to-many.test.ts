24ce431a6a252bc89cc70d882486ffd11a9f75ac
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-03 14:54:54 +0200

    Adds integration tests for updating a "manyOf" relational property

--- test/relations/one-to-many.test.ts
+++ test/relations/one-to-many.test.ts
@@ -91,3 +91,111 @@ test('should not throw error if an entity with one-to-many relation is created w
 
   expect(() => db.user.create()).not.toThrow()
 })
+
+test('updates the relational value via the ".update()" model method', () => {
+  const db = factory({
+    user: {
+      id: primaryKey(random.uuid),
+      posts: manyOf('post'),
+    },
+    post: {
+      id: primaryKey(random.uuid),
+      title: random.words,
+    },
+  })
+
+  const firstPost = db.post.create({ title: 'First post' })
+  const secondPost = db.post.create({ title: 'Second post' })
+  const user = db.user.create({
+    id: 'abc-123',
+    posts: [firstPost],
+  })
+  const refetchUser = () => {
+    return db.user.findFirst({
+      where: {
+        id: { equals: 'abc-123' },
+      },
+    })
+  }
+
+  expect(user.posts).toEqual([firstPost])
+  expect(refetchUser()).toEqual({
+    id: 'abc-123',
+    posts: [firstPost],
+  })
+
+  // Update the "posts" relational property.
+  const updatedUser = db.user.update({
+    where: {
+      id: { equals: 'abc-123' },
+    },
+    data: {
+      posts: [secondPost],
+    },
+  })
+
+  expect(updatedUser).toEqual({
+    id: 'abc-123',
+    posts: [secondPost],
+  })
+  expect(refetchUser()).toEqual({
+    id: 'abc-123',
+    posts: [secondPost],
+  })
+})
+
+test('updates the relational value via a compatible object', () => {
+  const db = factory({
+    user: {
+      id: primaryKey(random.uuid),
+      posts: manyOf('post'),
+    },
+    post: {
+      id: primaryKey(random.uuid),
+      title: random.words,
+    },
+  })
+  const firstPost = db.post.create({ title: 'First post' })
+  const user = db.user.create({
+    id: 'abc-123',
+    posts: [firstPost],
+  })
+  const refetchUser = () => {
+    return db.user.findFirst({
+      where: {
+        id: { equals: 'abc-123' },
+      },
+    })
+  }
+
+  expect(user.posts).toEqual([firstPost])
+  expect(refetchUser()).toEqual({
+    id: 'abc-123',
+    posts: [firstPost],
+  })
+
+  // Update the "posts" relational property
+  // with a compatible direct Object.
+  const directObject = {
+    id: 'post-1',
+    title: 'Compatible post',
+  }
+
+  const updatedUser = db.user.update({
+    where: {
+      id: { equals: 'abc-123' },
+    },
+    data: {
+      posts: [directObject],
+    },
+  })
+
+  expect(updatedUser).toEqual({
+    id: 'abc-123',
+    posts: [directObject],
+  })
+  expect(refetchUser()).toEqual({
+    id: 'abc-123',
+    posts: [directObject],
+  })
+})
