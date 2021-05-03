5cdbe647b24087091cb3f71a51011b445655bb4a
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-03 15:02:46 +0200

    Adds update tests to the "one-to-one" relations

--- test/relations/one-to-one.test.ts
+++ test/relations/one-to-one.test.ts
@@ -73,3 +73,122 @@ test('should not throw error if an entity with one-to-one relation is created wi
 
   expect(() => db.capital.create()).not.toThrow()
 })
+
+test('updates the relational property to the next entity', () => {
+  const db = factory({
+    country: {
+      name: primaryKey(random.words),
+    },
+    capital: {
+      name: primaryKey(random.word),
+      country: oneOf('country'),
+    },
+  })
+  const refetchCapital = () => {
+    return db.capital.findFirst({
+      where: {
+        name: { equals: 'Washington' },
+      },
+    })
+  }
+
+  const usa = db.country.create({
+    name: 'United States of America',
+  })
+  const australia = db.country.create({
+    name: 'Australia',
+  })
+  db.capital.create({
+    name: 'Washington',
+    country: usa,
+  })
+  expect(refetchCapital()).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'United States of America',
+    },
+  })
+
+  // Update the "country" relational property.
+  const updatedCapital = db.capital.update({
+    where: {
+      name: { equals: 'Washington' },
+    },
+    data: {
+      country: australia,
+    },
+  })
+
+  expect(updatedCapital).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'Australia',
+    },
+  })
+  expect(refetchCapital()).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'Australia',
+    },
+  })
+})
+
+test('updates the relational property to a compatible object value', () => {
+  const db = factory({
+    country: {
+      name: primaryKey(random.words),
+    },
+    capital: {
+      name: primaryKey(random.word),
+      country: oneOf('country'),
+    },
+  })
+  const refetchCapital = () => {
+    return db.capital.findFirst({
+      where: {
+        name: { equals: 'Washington' },
+      },
+    })
+  }
+
+  const usa = db.country.create({
+    name: 'United States of America',
+  })
+  db.capital.create({
+    name: 'Washington',
+    country: usa,
+  })
+
+  expect(refetchCapital()).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'United States of America',
+    },
+  })
+
+  // Update the "country" relational property
+  // to a compatible object value.
+  const updatedCapital = db.capital.update({
+    where: {
+      name: { equals: 'Washington' },
+    },
+    data: {
+      country: {
+        name: 'Australia',
+      },
+    },
+  })
+
+  expect(updatedCapital).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'Australia',
+    },
+  })
+  expect(refetchCapital()).toEqual({
+    name: 'Washington',
+    country: {
+      name: 'Australia',
+    },
+  })
+})
