d9b9f32cc5aec4f14885ab94a5d4c857a6ddbf9d
Kaiwen Sun <skw_kevin@126.com>
2021-06-28 15:18:42 -0700

    handle null value in db isInternalEntity

--- test/db/drop.test.ts
+++ test/db/drop.test.ts
@@ -1,4 +1,4 @@
-import { drop, factory, identity, primaryKey } from '@mswjs/data'
+import { drop, factory, identity, primaryKey, oneOf } from '@mswjs/data'
 
 test('drops all records in the database', () => {
   const db = factory({
@@ -34,4 +34,25 @@ test('does nothing when the database is already empty', () => {
 
   drop(db)
   expect(db.user.getAll()).toHaveLength(0)
+})
+
+test('db drop does not care relation dependency', () => {
+  const db = factory({
+    user: {
+      id: primaryKey(identity('abc-123')),
+    },
+    group: {
+      id: primaryKey(identity('xyz-123')),
+      root: oneOf('user')
+    }
+  })
+
+  const user = db.user.create()
+  db.group.create({root: user})
+  expect(db.user.getAll()).toHaveLength(1)
+  expect(db.group.getAll()).toHaveLength(1)
+
+  drop(db)
+  expect(db.user.getAll()).toHaveLength(0)
+  expect(db.group.getAll()).toHaveLength(0)
 })
