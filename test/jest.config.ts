fddcd65e20fccc51b52b57062d44c7bc5fc43468
Artem Zakharchenko <kettanaito@gmail.com>
2021-12-03 11:53:11 +0100

    chore: add custom "toHaveRelationalProperty" jest matcher

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -2,6 +2,7 @@ export default {
   preset: 'ts-jest',
   testTimeout: 60000,
   moduleNameMapper: {
-    '^@mswjs/data(.*)': '<rootDir>/$1',
+    '^@mswjs/data(.*)': '<rootDir>/../$1',
   },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 }
