6fc6e33118faa8f13ae72ba4277edde1fcb8181e
Artem Zakharchenko <kettanaito@gmail.com>
2022-02-05 12:18:59 +0100

    feat: exports "PRIMARY_KEY" and "ENTITY_TYPE" symbols

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export { factory } from './factory'
 export { primaryKey } from './primaryKey'
-export { nullable } from './nullable';
+export { nullable } from './nullable'
 export { oneOf } from './relations/oneOf'
 export { manyOf } from './relations/manyOf'
 export { drop } from './db/drop'
 export { identity } from './utils/identity'
+
+/* Types */
+export { PRIMARY_KEY, ENTITY_TYPE } from './glossary'
