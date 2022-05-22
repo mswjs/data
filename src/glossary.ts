2c9d39cb903606bdd328bc3a5dfd5cb2687263ff
Aloys Berger <alo@reaktive.com>
2022-05-22 13:25:49 +1000

    fix(modelvaluetype definition): review the ModelValueType definition using exisiting type AnyObject
    Change the definition of ModelValueType from 'Object' to 'AnyObject' (an existing type of the
    library)
    
    re 203

--- src/glossary.ts
+++ src/glossary.ts
@@ -19,7 +19,10 @@ export type KeyType = string | number | symbol
 export type AnyObject = Record<KeyType, any>
 export type PrimaryKeyType = string | number
 export type PrimitiveValueType = string | number | boolean | Date
-export type ModelValueType = PrimitiveValueType | PrimitiveValueType[] | Object
+export type ModelValueType =
+  | PrimitiveValueType
+  | PrimitiveValueType[]
+  | AnyObject
 export type ModelValueTypeGetter = () => ModelValueType
 
 export type ModelDefinition = Record<string, ModelDefinitionValue>
