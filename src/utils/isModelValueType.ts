704c03aec83a92026ba707cbb7350ea4f1afc7c2
StanleySathler <stanleysathlerpinto@gmail.com>
2023-09-10 11:44:04 -0300

    chore: reuse utility

--- src/utils/isModelValueType.ts
+++ src/utils/isModelValueType.ts
@@ -1,16 +1,17 @@
 import isObjectLike from 'lodash/isObjectLike'
 import { ModelValueType, PrimitiveValueType } from '../glossary'
+import { isObject } from './isObject'
 
 function isPrimitiveValueType(value: any): value is PrimitiveValueType {
   return (
     typeof value === 'string' ||
     typeof value === 'number' ||
     typeof value === 'boolean' ||
-    isObjectLike(value) ||
+    isObject(value) ||
     value?.constructor?.name === 'Date'
   )
 }
 
 export function isModelValueType(value: any): value is ModelValueType {
-  return isPrimitiveValueType(value)
+  return isPrimitiveValueType(value) || Array.isArray(value)
 }
